3f894d89d393c7504adf50b97fae8a27ca9c0ebe
pasindu uduwela <puduwela@icptechno.com>
2023-11-23 10:37:02 +0530

    JPA Entity and repository classes added

--- src/main/java/com/example/SpringSecuritySample/SpringSecuritySampleApplication.java
+++ src/main/java/com/example/SpringSecuritySample/SpringSecuritySampleApplication.java
@@ -2,9 +2,13 @@ package com.example.SpringSecuritySample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.SpringSecuritySample.repository")
+@EntityScan("com.example.SpringSecuritySample.model")
 //@ComponentScan("com.example.SpringSecuritySample.controller")//optional
 public class SpringSecuritySampleApplication {
 
