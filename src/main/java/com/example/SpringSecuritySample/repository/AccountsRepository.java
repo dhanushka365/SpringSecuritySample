75e0ee5ca5fc99dd27f5832cea86800d143f0944
pasindu uduwela <puduwela@icptechno.com>
2023-11-24 12:49:48 +0530

    changes commited

---
+++ src/main/java/com/example/SpringSecuritySample/repository/AccountsRepository.java
@@ -0,0 +1,10 @@
+package com.example.SpringSecuritySample.repository;
+
+import com.example.SpringSecuritySample.model.Accounts;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends CrudRepository<Accounts, Long> {
+    Accounts findByCustomerId(int customerId);
+}
---
+++ src/main/java/com/example/SpringSecuritySample/repository/ContactRepository.java
@@ -0,0 +1,10 @@
+package com.example.SpringSecuritySample.repository;
+
+import com.example.SpringSecuritySample.model.Contact;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends CrudRepository<Contact, Long> {
+
+}
